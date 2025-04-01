94afd06d5227ffd79a6becee67411d5a8224d0c4
Leandro <leandro.leon@stocksnips.net>
2025-04-01 14:50:07 +0000

    cont 3

--- src/perturbench/modelcore/train.py
+++ src/perturbench/modelcore/train.py
@@ -93,7 +93,8 @@ def train(runtime_context: dict):
                 ckpt_path = "best"
         else:
             ckpt_path = cfg.get("ckpt_path")
-        trainer.test(model=model.model, datamodule=datamodule, ckpt_path=ckpt_path)
+        set_trace()
+        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
         summary_metrics_dict = model.summary_metrics.to_dict()[
             model.summary_metrics.columns[0]
         ]
