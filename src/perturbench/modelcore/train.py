091278348e23638ad025ad5c0c5789b6ec3d1fb3
Leandro <leandro.leon@stocksnips.net>
2025-04-01 09:33:46 -0400

    cont

--- src/perturbench/modelcore/train.py
+++ src/perturbench/modelcore/train.py
@@ -76,7 +76,7 @@ def train(runtime_context: dict):
 
     if cfg.get("train"):
         log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+        trainer.fit(model=model.model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
 
     train_metrics = trainer.callback_metrics
 
